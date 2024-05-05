e4ee57f1b3379f9e80ef5ec1a496463526cf6bf2
mhezzet <mhezzet@gmail.com>
2024-05-05 15:22:41 +0300

    change the title

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Poppins } from 'next/font/google';
-
 import { LoginButton } from '@/components/auth/login-button';
 import { cn } from '@/lib/utils';
 
@@ -13,7 +12,7 @@ export default async function Home() {
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <div className="space-y-6 text-center">
         <h1
-          className={cn('text-6xl font-semibold text-white drop-shadow-md', poppinsFont.className)}
+          className={cn('text-6xl font-semibold text-white drop-shadow-md ', poppinsFont.className)}
         >
           🔐Auth
         </h1>
